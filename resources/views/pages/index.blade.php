b8560247430854c31dfd06fcfb2071e6f9db33d5
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-02-12 16:03:01 +0100

    add: added movies info with db call

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,17 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Movies</h1>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @foreach ($movies as $movie)
+        <div class="col">
+            <div class="card h-100">
+                {{$movie -> title }}
+                {{$movie -> nationality }}
+                {{$movie -> date }}
+                {{$movie -> vote }}
+            </div>
+        </div> 
+        @endforeach
+    </div>
 @endsection
